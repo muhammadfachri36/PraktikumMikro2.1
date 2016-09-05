86e6465712b6dd75a05c36df189220cbc9a7efea
muhammadfachri36 <mamadfahri4@gmail.com>
2016-09-05 15:44:38 +0700

    Add files via upload

---
+++ Praktikum2.1.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, 1);
+  pinMode(12, 1);
+  pinMode(13, 1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, 1);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, 0);
+}
